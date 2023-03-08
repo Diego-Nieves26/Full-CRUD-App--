b513cb86101931c37e289e7e9076f00f6cafda47
Diego Nieves <nieves.diego0426@gmail.com>
2023-03-07 19:14:49 -0500

    added readme

--- client/src/store/slices/userSelected.slice.js
+++ client/src/store/slices/userSelected.slice.js
@@ -3,7 +3,7 @@ import { setIsLoading } from "./isLoading.slice";
 import axios from "axios";
 import { getUsers } from "./users.slice";
 
-const API = "https://proyect-crud-api.herokuapp.com/api/v1/users/";
+const API = "https://full-crud-app-back.onrender.com/api/v1/users/";
 
 export const userSelectedSlice = createSlice({
   name: "userSelected",
